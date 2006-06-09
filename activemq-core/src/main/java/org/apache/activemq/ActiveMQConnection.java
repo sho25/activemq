74835f000b4729b5e7f3e18f18d4128d106aa956
Robert Davies <rajdavies@apache.org>
2006-06-09 07:38:25 +0000

    set default for optimize acknowledge to false For performance - recommend use DUPS_OK acknowledge - at least its clear what your going to get - and it performs better too
    git-svn-id: https://svn.apache.org/repos/asf/incubator/activemq/trunk@412964 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 7a386be93891dc331260160d0d76ced959f17f25

--- activemq-core/src/main/java/org/apache/activemq/ActiveMQConnection.java
+++ activemq-core/src/main/java/org/apache/activemq/ActiveMQConnection.java
@@ -1177,7 +1177,7 @@ specifier|private
 name|boolean
 name|optimizeAcknowledge
 init|=
-literal|true
+literal|false
 decl_stmt|;
 specifier|private
 name|boolean
--- activemq-core/src/main/java/org/apache/activemq/ActiveMQConnectionFactory.java
+++ activemq-core/src/main/java/org/apache/activemq/ActiveMQConnectionFactory.java
@@ -505,7 +505,7 @@ specifier|private
 name|boolean
 name|optimizeAcknowledge
 init|=
-literal|true
+literal|false
 decl_stmt|;
 specifier|private
 name|int
