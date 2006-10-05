ba203f2ee66e62c33d068c4bb01026ab17dbbb0e
Robert Davies <rajdavies@apache.org>
2006-10-05 07:20:31 +0000

    work for https://issues.apache.org/activemq/browse/AMQ-876
    git-svn-id: https://svn.apache.org/repos/asf/incubator/activemq/trunk@453124 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 6e8044251947ebb95ad504646144c85228d9711b

---
+++ activemq-core/src/main/java/org/apache/activemq/kaha/StoreLocation.java
@@ -0,0 +1,49 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/**  *   * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements. See the NOTICE file distributed with this  * work for additional information regarding copyright ownership. The ASF  * licenses this file to You under the Apache License, Version 2.0 (the  * "License"); you may not use this file except in compliance with the License.  * You may obtain a copy of the License at  *   * http://www.apache.org/licenses/LICENSE-2.0  *   * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the  * License for the specific language governing permissions and limitations under  * the License.  */
+end_comment
+
+begin_package
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|activemq
+operator|.
+name|kaha
+package|;
+end_package
+
+begin_comment
+comment|/**  * Location of a data in the Store  *   * @version $Revision: 1.2 $  */
+end_comment
+
+begin_interface
+specifier|public
+interface|interface
+name|StoreLocation
+block|{
+comment|/**      * @return Returns the size.      */
+specifier|public
+name|int
+name|getSize
+parameter_list|()
+function_decl|;
+comment|/**      * @return Returns the offset.      */
+specifier|public
+name|long
+name|getOffset
+parameter_list|()
+function_decl|;
+comment|/**      * @return Returns the file.      */
+specifier|public
+name|int
+name|getFile
+parameter_list|()
+function_decl|;
+block|}
+end_interface
+
+end_unit
+
