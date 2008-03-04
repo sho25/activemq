ec52a49e39dab7b28ae0153bf2c47d43ba1dd251
Hiram R. Chirino <chirino@apache.org>
2008-03-04 19:36:58 +0000

    The AsyncDataManager now supports doing a callback notification for when a write has been secured to disk.
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@633603 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: a9b841af61afb84936475b1eaddd7b5eeb285620

--- activemq-core/src/main/java/org/apache/activemq/kaha/impl/async/DataFileAccessor.java
+++ activemq-core/src/main/java/org/apache/activemq/kaha/impl/async/DataFileAccessor.java
@@ -51,18 +51,6 @@ end_import
 
 begin_import
 import|import
-name|java
-operator|.
-name|util
-operator|.
-name|concurrent
-operator|.
-name|ConcurrentHashMap
-import|;
-end_import
-
-begin_import
-import|import
 name|org
 operator|.
 name|apache
