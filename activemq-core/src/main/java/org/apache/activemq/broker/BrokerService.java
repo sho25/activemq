b2b8e698c609ad532f430982a69f6da26592c2fe
Robert Davies <rajdavies@apache.org>
2008-04-09 15:41:33 +0000

    Throw an exception if advisory messages are disabled when creating networks
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@646409 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 8bf9c17dd9ee51eaed6a9ef3c6affe2c7bd718bc

--- activemq-core/src/main/java/org/apache/activemq/broker/BrokerService.java
+++ activemq-core/src/main/java/org/apache/activemq/broker/BrokerService.java
@@ -1667,6 +1667,25 @@ parameter_list|)
 throws|throws
 name|Exception
 block|{
+if|if
+condition|(
+operator|!
+name|isAdvisorySupport
+argument_list|()
+condition|)
+block|{
+throw|throw
+operator|new
+name|javax
+operator|.
+name|jms
+operator|.
+name|IllegalStateException
+argument_list|(
+literal|"Networks require advisory messages to function - advisories are currently disabled"
+argument_list|)
+throw|;
+block|}
 name|NetworkConnector
 name|connector
 init|=
