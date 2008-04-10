c1e3d8b80481fd1c78b187472e2f0d1730a773c2
Robert Davies <rajdavies@apache.org>
2008-04-10 13:16:37 +0000

    Applied patch from https://issues.apache.org/activemq/browse/AMQ-1345
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@646789 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: acf453b46a7fcb03a59af99142eec8a9a7ee6be1

--- activemq-ra/src/main/java/org/apache/activemq/ra/MessageResourceAdapter.java
+++ activemq-ra/src/main/java/org/apache/activemq/ra/MessageResourceAdapter.java
@@ -74,7 +74,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * Knows how to connect to one ActiveMQ server. It can then activate endpoints  * and deliver messages to those end points using the connection configure in  * the resource adapter.<p/>Must override equals and hashCode (JCA spec 16.4)  *   * @org.apache.xbean.XBean element="resourceAdapter" rootElement="true"  *                         description="The JCA Resource Adaptor for ActiveMQ"  * @version $Revision$  */
+comment|/**  * Knows how to connect to one ActiveMQ server. It can then activate endpoints  * and deliver messages to those end points using the connection configure in  * the resource adapter.<p/>Must override equals and hashCode (JCA spec 16.4)  *   * @version $Revision$  */
 end_comment
 
 begin_interface
