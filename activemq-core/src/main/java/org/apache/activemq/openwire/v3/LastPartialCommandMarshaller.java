52647642c82aa1da5a8c925354689389b4e59fcb
Hiram R. Chirino <chirino@apache.org>
2008-05-28 21:53:17 +0000

    Reverting recent chagnes to openwire v3 marshallers that snuck in with my last commit
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@661104 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: b3ee59d7a106105ac3aff62a72921f9f740b7999

--- activemq-core/src/main/java/org/apache/activemq/openwire/v3/LastPartialCommandMarshaller.java
+++ activemq-core/src/main/java/org/apache/activemq/openwire/v3/LastPartialCommandMarshaller.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/**  *  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  * http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -55,9 +55,9 @@ name|apache
 operator|.
 name|activemq
 operator|.
-name|openwire
+name|command
 operator|.
-name|*
+name|DataStructure
 import|;
 end_import
 
@@ -71,7 +71,35 @@ name|activemq
 operator|.
 name|command
 operator|.
-name|*
+name|LastPartialCommand
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|activemq
+operator|.
+name|openwire
+operator|.
+name|BooleanStream
+import|;
+end_import
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|activemq
+operator|.
+name|openwire
+operator|.
+name|OpenWireFormat
 import|;
 end_import
 
