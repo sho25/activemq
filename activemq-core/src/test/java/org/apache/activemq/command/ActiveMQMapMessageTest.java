cc947c8c71c9b124a08c63da98ed6ad800a036c0
Robert Davies <rajdavies@apache.org>
2009-07-07 05:34:00 +0000

    Added fix for https://issues.apache.org/activemq/browse/AMQ-1282
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@791708 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 8c70e4a78055dce69faef0bf718d36120666c1b7

--- activemq-core/src/test/java/org/apache/activemq/command/ActiveMQMapMessageTest.java
+++ activemq-core/src/test/java/org/apache/activemq/command/ActiveMQMapMessageTest.java
@@ -2445,16 +2445,27 @@ name|getContent
 argument_list|()
 argument_list|)
 expr_stmt|;
-name|assertNull
-argument_list|(
+try|try
+block|{
 name|mapMessage
 operator|.
 name|getString
 argument_list|(
 literal|"String"
 argument_list|)
+expr_stmt|;
+name|fail
+argument_list|(
+literal|"Should throw a Null pointer"
 argument_list|)
 expr_stmt|;
+block|}
+catch|catch
+parameter_list|(
+name|NullPointerException
+name|e
+parameter_list|)
+block|{                      }
 name|mapMessage
 operator|.
 name|clearBody
