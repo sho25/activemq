678f1f090b17d7c7b7208f32416996dceae196ea
Gary Tully <gtully@apache.org>
2011-08-18 10:49:17 +0000

    use autofail
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@1159153 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: c2179d15b72683be9217fe2f46defbde3d39aa07

--- activemq-core/src/test/java/org/apache/activemq/OnePrefetchAsyncConsumerTest.java
+++ activemq-core/src/test/java/org/apache/activemq/OnePrefetchAsyncConsumerTest.java
@@ -437,6 +437,11 @@ parameter_list|()
 throws|throws
 name|Exception
 block|{
+name|setAutoFail
+argument_list|(
+literal|true
+argument_list|)
+expr_stmt|;
 name|bindAddress
 operator|=
 literal|"tcp://localhost:0"
