651edbd7075699bc28e0ff6410e262ed7c97c12e
Timothy A. Bish <tabish@apache.org>
2011-09-03 21:39:12 +0000

    Some updates and changes to support some work on https://issues.apache.org/jira/browse/AMQ-3467
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@1164936 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 82e3be347cc78316b759f5d196b8423b9a305a9f

--- kahadb/src/main/java/org/apache/kahadb/index/ListNode.java
+++ kahadb/src/main/java/org/apache/kahadb/index/ListNode.java
@@ -1326,7 +1326,11 @@ block|}
 annotation|@
 name|SuppressWarnings
 argument_list|(
+block|{
 literal|"unchecked"
+block|,
+literal|"rawtypes"
+block|}
 argument_list|)
 specifier|public
 name|ListNode
