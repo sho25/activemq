de85301e23c6839a264fe9a018bc52951eae1e06
Timothy A. Bish <tabish@apache.org>
2011-09-30 19:38:46 +0000

    Add missing license header
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@1177799 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 0890672e087d95f3a9fa84fb62fe460d476f4609

--- activemq-core/src/test/java/org/apache/activemq/bugs/AMQ3014Test.java
+++ activemq-core/src/test/java/org/apache/activemq/bugs/AMQ3014Test.java
@@ -1,4 +1,8 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+
 begin_package
 package|package
 name|org
@@ -603,7 +607,7 @@ argument_list|()
 expr_stmt|;
 block|}
 block|}
-comment|/** 	 * This test verifies that the local broker's ID is typically known by the 	 * bridge support before the local broker's BrokerInfo is sent to the remote 	 * broker. 	 */
+comment|/**      * This test verifies that the local broker's ID is typically known by the      * bridge support before the local broker's BrokerInfo is sent to the remote      * broker.      */
 annotation|@
 name|Test
 specifier|public
@@ -621,7 +625,7 @@ literal|3000
 argument_list|)
 expr_stmt|;
 block|}
-comment|/** 	 * This test verifies that timing can arise under which the local broker's 	 * ID is not known by the bridge support before the local broker's 	 * BrokerInfo is sent to the remote broker. 	 */
+comment|/**      * This test verifies that timing can arise under which the local broker's      * ID is not known by the bridge support before the local broker's      * BrokerInfo is sent to the remote broker.      */
 annotation|@
 name|Test
 specifier|public
