f7480332bb48a08561e709602ac483d2828939ce
Timothy A. Bish <tabish@apache.org>
2011-12-22 15:15:48 +0000

    apply patch and add test for: https://issues.apache.org/jira/browse/AMQ-3436
    Some modifications needed for the patch to work correctly.
    
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@1222275 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: cb1b92bf58e2631077a31b97cc8087bb35605a03

--- activemq-core/src/main/java/org/apache/activemq/broker/region/cursors/PendingList.java
+++ activemq-core/src/main/java/org/apache/activemq/broker/region/cursors/PendingList.java
@@ -25,6 +25,16 @@ name|java
 operator|.
 name|util
 operator|.
+name|Collection
+import|;
+end_import
+
+begin_import
+import|import
+name|java
+operator|.
+name|util
+operator|.
 name|Iterator
 import|;
 end_import
@@ -49,17 +59,25 @@ begin_interface
 specifier|public
 interface|interface
 name|PendingList
+extends|extends
+name|Iterable
+argument_list|<
+name|MessageReference
+argument_list|>
 block|{
+comment|/**      * Returns true if there are no Messages in the PendingList currently.      * @return true if the PendingList is currently empty.      */
 specifier|public
 name|boolean
 name|isEmpty
 parameter_list|()
 function_decl|;
+comment|/**      * Discards all Messages currently held in the PendingList.      */
 specifier|public
 name|void
 name|clear
 parameter_list|()
 function_decl|;
+comment|/**      * Adds the given message to the head of the list.      *      * @param message      *      The MessageReference that is to be added to this list.      *      * @return the PendingNode that contains the newly added message.      */
 specifier|public
 name|PendingNode
 name|addMessageFirst
@@ -68,6 +86,7 @@ name|MessageReference
 name|message
 parameter_list|)
 function_decl|;
+comment|/**      * Adds the given message to the tail of the list.      *      * @param message      *      The MessageReference that is to be added to this list.      *      * @return the PendingNode that contains the newly added message.      */
 specifier|public
 name|PendingNode
 name|addMessageLast
@@ -76,6 +95,7 @@ name|MessageReference
 name|message
 parameter_list|)
 function_decl|;
+comment|/**      * Removes the given MessageReference from the PendingList if it is      * contained within.      *      * @param message      *      The MessageReference that is to be removed to this list.      *      * @return the PendingNode that contains the removed message or null if the      *         message was not present in this list.      */
 specifier|public
 name|PendingNode
 name|remove
@@ -84,11 +104,13 @@ name|MessageReference
 name|message
 parameter_list|)
 function_decl|;
+comment|/**      * Returns the number of MessageReferences that are awaiting dispatch.      * @return current count of the pending messages.      */
 specifier|public
 name|int
 name|size
 parameter_list|()
 function_decl|;
+comment|/**      * Returns an iterator over the pending Messages.  The subclass controls how      * the returned iterator actually traverses the list of pending messages allowing      * for the order to vary based on factors like Message priority or some other      * mechanism.      *      * @return an Iterator that returns MessageReferences contained in this list.      */
 specifier|public
 name|Iterator
 argument_list|<
@@ -96,6 +118,33 @@ name|MessageReference
 argument_list|>
 name|iterator
 parameter_list|()
+function_decl|;
+comment|/**      * Query the PendingList to determine if the given message is contained within.      *      * @param message      *      The Message that is the target of this query.      *      * @return true if the MessageReference is contained in this list.      */
+specifier|public
+name|boolean
+name|contains
+parameter_list|(
+name|MessageReference
+name|message
+parameter_list|)
+function_decl|;
+comment|/**      * Returns a new Collection that contains all the MessageReferences currently      * held in this PendingList.  The elements of the list are ordered using the      * same rules as the subclass uses for iteration.      *      * @return a new Collection containing this lists MessageReferences.      */
+specifier|public
+name|Collection
+argument_list|<
+name|MessageReference
+argument_list|>
+name|values
+parameter_list|()
+function_decl|;
+comment|/**      * Adds all the elements of the given PendingList to this PendingList.      *      * @param pendingList      *      The PendingList that is to be added to this collection.      */
+specifier|public
+name|void
+name|addAll
+parameter_list|(
+name|PendingList
+name|pendingList
+parameter_list|)
 function_decl|;
 block|}
 end_interface
