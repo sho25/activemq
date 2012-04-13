0442561524737b9ed81db5275eba0a5a83e88cbb
Timothy A. Bish <tabish@apache.org>
2012-04-13 22:21:27 +0000

    apply fix for: https://issues.apache.org/jira/browse/AMQ-3803
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@1325985 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: f81a790ff5cb13eddb3e46360a0a7aa75b985be1

--- activemq-core/src/main/java/org/apache/activemq/jndi/ActiveMQWASInitialContextFactory.java
+++ activemq-core/src/main/java/org/apache/activemq/jndi/ActiveMQWASInitialContextFactory.java
@@ -66,7 +66,7 @@ import|;
 end_import
 
 begin_comment
-comment|/**  * This implementation of<CODE>InitialContextFactory</CODE> should be used  * when ActiveMQ is used as WebSphere Generic JMS Provider. It is proved that it  * works on WebSphere 5.1. The reason for using this class is that custom  * property defined for Generic JMS Provider are passed to InitialContextFactory  * only if it begins with java.naming or javax.naming prefix. Additionaly  * provider url for the JMS provider can not contain ',' character that is  * necessary when the list of nodes is provided. So the role of this class is to  * transform properties before passing it to  *<CODE>ActiveMQInitialContextFactory</CODE>.  *   * @author Pawel Tucholski  */
+comment|/**  * This implementation of<CODE>InitialContextFactory</CODE> should be used  * when ActiveMQ is used as WebSphere Generic JMS Provider. It is proved that it  * works on WebSphere 5.1. The reason for using this class is that custom  * property defined for Generic JMS Provider are passed to InitialContextFactory  * only if it begins with java.naming or javax.naming prefix. Additionaly  * provider url for the JMS provider can not contain ',' character that is  * necessary when the list of nodes is provided. So the role of this class is to  * transform properties before passing it to  *<CODE>ActiveMQInitialContextFactory</CODE>.  *  * @author Pawel Tucholski  */
 end_comment
 
 begin_class
@@ -99,7 +99,7 @@ argument_list|)
 argument_list|)
 return|;
 block|}
-comment|/**      * Performs following transformation of properties:      *<ul>      *<li>(java.naming.queue.xxx.yyy,value)=>(queue.xxx/yyy,value)      *<li>(java.naming.topic.xxx.yyy,value)=>(topic.xxx/yyy,value)      *<li>(java.naming.connectionFactoryNames,value)=>(connectionFactoryNames,value)      *<li>(java.naming.provider.url,url1;url2)=>java.naming.provider.url,url1,url1)      *<ul>      *       * @param environment properties for transformation      * @return environment after transformation      */
+comment|/**      * Performs following transformation of properties:      *<ul>      *<li>(java.naming.queue.xxx.yyy,value)=>(queue.xxx/yyy,value)      *<li>(java.naming.topic.xxx.yyy,value)=>(topic.xxx/yyy,value)      *<li>(java.naming.connectionFactoryNames,value)=>(connectionFactoryNames,value)      *<li>(java.naming.provider.url,url1;url2)=>java.naming.provider.url,url1,url1)      *<ul>      *      * @param environment properties for transformation      * @return environment after transformation      */
 specifier|protected
 name|Hashtable
 name|transformEnvironment
@@ -149,6 +149,23 @@ operator|.
 name|next
 argument_list|()
 decl_stmt|;
+if|if
+condition|(
+name|entry
+operator|.
+name|getKey
+argument_list|()
+operator|instanceof
+name|String
+operator|&&
+name|entry
+operator|.
+name|getValue
+argument_list|()
+operator|instanceof
+name|String
+condition|)
+block|{
 name|String
 name|key
 init|=
@@ -345,7 +362,7 @@ name|PROVIDER_URL
 argument_list|)
 condition|)
 block|{
-comment|// websphere administration console does not exept , character
+comment|// websphere administration console does not accept the , character
 comment|// in provider url, so ; must be used
 comment|// all ; to ,
 name|value
@@ -382,6 +399,7 @@ argument_list|,
 name|value
 argument_list|)
 expr_stmt|;
+block|}
 block|}
 block|}
 return|return
