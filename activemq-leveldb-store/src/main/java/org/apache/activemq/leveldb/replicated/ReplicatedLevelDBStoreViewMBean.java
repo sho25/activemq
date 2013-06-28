f6e4b51d1b729a71c29858f3a4cd2f0c4bcaeb61
Hiram R. Chirino <chirino@apache.org>
2013-06-28 16:57:29 +0000

    Expose the replicated store status via JMX.
    git-svn-id: https://svn.apache.org/repos/asf/activemq/trunk@1497843 13f79535-47bb-0310-9956-ffa450edef68
    
    Former-commit-id: 16cd8c3954c40c2b83c8692fd24b35e272b6ee0e

---
+++ activemq-leveldb-store/src/main/java/org/apache/activemq/leveldb/replicated/ReplicatedLevelDBStoreViewMBean.java
@@ -0,0 +1,238 @@
+begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
+begin_comment
+comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+end_comment
+
+begin_package
+package|package
+name|org
+operator|.
+name|apache
+operator|.
+name|activemq
+operator|.
+name|leveldb
+operator|.
+name|replicated
+package|;
+end_package
+
+begin_import
+import|import
+name|org
+operator|.
+name|apache
+operator|.
+name|activemq
+operator|.
+name|broker
+operator|.
+name|jmx
+operator|.
+name|MBeanInfo
+import|;
+end_import
+
+begin_comment
+comment|/**  *<p>  *</p>  *  * @author<a href="http://hiramchirino.com">Hiram Chirino</a>  */
+end_comment
+
+begin_interface
+specifier|public
+interface|interface
+name|ReplicatedLevelDBStoreViewMBean
+block|{
+annotation|@
+name|MBeanInfo
+argument_list|(
+literal|"The address of the ZooKeeper server."
+argument_list|)
+name|String
+name|getZkAddress
+parameter_list|()
+function_decl|;
+annotation|@
+name|MBeanInfo
+argument_list|(
+literal|"The path in ZooKeeper to hold master elections."
+argument_list|)
+name|String
+name|getZkPath
+parameter_list|()
+function_decl|;
+annotation|@
+name|MBeanInfo
+argument_list|(
+literal|"The ZooKeeper session timeout."
+argument_list|)
+name|String
+name|getZkSessionTmeout
+parameter_list|()
+function_decl|;
+annotation|@
+name|MBeanInfo
+argument_list|(
+literal|"The address and port the master will bind for the replication protocol."
+argument_list|)
+name|String
+name|getBind
+parameter_list|()
+function_decl|;
+annotation|@
+name|MBeanInfo
+argument_list|(
+literal|"The number of replication nodes that will be part of the replication cluster."
+argument_list|)
+name|int
+name|getReplicas
+parameter_list|()
+function_decl|;
+annotation|@
+name|MBeanInfo
+argument_list|(
+literal|"The role of this node in the replication cluster."
+argument_list|)
+name|String
+name|getNodeRole
+parameter_list|()
+function_decl|;
+annotation|@
+name|MBeanInfo
+argument_list|(
+literal|"The replication status."
+argument_list|)
+name|String
+name|getStatus
+parameter_list|()
+function_decl|;
+annotation|@
+name|MBeanInfo
+argument_list|(
+literal|"The current position of the replication log."
+argument_list|)
+name|Long
+name|getPosition
+parameter_list|()
+function_decl|;
+annotation|@
+name|MBeanInfo
+argument_list|(
+literal|"The directory holding the data."
+argument_list|)
+name|String
+name|getDirectory
+parameter_list|()
+function_decl|;
+annotation|@
+name|MBeanInfo
+argument_list|(
+literal|"The size the log files are allowed to grow to."
+argument_list|)
+name|long
+name|getLogSize
+parameter_list|()
+function_decl|;
+annotation|@
+name|MBeanInfo
+argument_list|(
+literal|"The implementation of the LevelDB index being used."
+argument_list|)
+name|String
+name|getIndexFactory
+parameter_list|()
+function_decl|;
+annotation|@
+name|MBeanInfo
+argument_list|(
+literal|"Is data verified against checksums as it's loaded back from disk."
+argument_list|)
+name|boolean
+name|getVerifyChecksums
+parameter_list|()
+function_decl|;
+annotation|@
+name|MBeanInfo
+argument_list|(
+literal|"The maximum number of open files the index will open at one time."
+argument_list|)
+name|int
+name|getIndexMaxOpenFiles
+parameter_list|()
+function_decl|;
+annotation|@
+name|MBeanInfo
+argument_list|(
+literal|"Number of keys between restart points for delta encoding of keys in the index"
+argument_list|)
+name|int
+name|getIndexBlockRestartInterval
+parameter_list|()
+function_decl|;
+annotation|@
+name|MBeanInfo
+argument_list|(
+literal|"Do aggressive checking of store data"
+argument_list|)
+name|boolean
+name|getParanoidChecks
+parameter_list|()
+function_decl|;
+annotation|@
+name|MBeanInfo
+argument_list|(
+literal|"Amount of data to build up in memory for the index before converting to a sorted on-disk file."
+argument_list|)
+name|int
+name|getIndexWriteBufferSize
+parameter_list|()
+function_decl|;
+annotation|@
+name|MBeanInfo
+argument_list|(
+literal|"Approximate size of user data packed per block for the index"
+argument_list|)
+name|int
+name|getIndexBlockSize
+parameter_list|()
+function_decl|;
+annotation|@
+name|MBeanInfo
+argument_list|(
+literal|"The type of compression to use for the index"
+argument_list|)
+name|String
+name|getIndexCompression
+parameter_list|()
+function_decl|;
+annotation|@
+name|MBeanInfo
+argument_list|(
+literal|"The size of the cache index"
+argument_list|)
+name|long
+name|getIndexCacheSize
+parameter_list|()
+function_decl|;
+annotation|@
+name|MBeanInfo
+argument_list|(
+literal|"The maximum amount of async writes to buffer up"
+argument_list|)
+name|int
+name|getAsyncBufferSize
+parameter_list|()
+function_decl|;
+annotation|@
+name|MBeanInfo
+argument_list|(
+literal|"The sync strategy to use."
+argument_list|)
+name|String
+name|getSync
+parameter_list|()
+function_decl|;
+block|}
+end_interface
+
+end_unit
+
