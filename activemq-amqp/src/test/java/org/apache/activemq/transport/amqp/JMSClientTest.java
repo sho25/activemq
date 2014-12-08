e3412ab95bdac5f3c8b57bda68dabbe5a7347589
Timothy Bish <tabish121@gmail.com>
2014-12-08 17:23:15 -0500

    https://issues.apache.org/jira/browse/AMQ-5475
    Ensure that client's connecting with non-supported AMQP versions or
    client's with invalid AMQP headers are sent an AMQP v1.0 header and are
    then disconnected.
    
    Former-commit-id: 61a3eab8ab4a1280c285229830820dea4db1d7f7

--- activemq-amqp/src/test/java/org/apache/activemq/transport/amqp/JMSClientTest.java
+++ activemq-amqp/src/test/java/org/apache/activemq/transport/amqp/JMSClientTest.java
@@ -18,6 +18,78 @@ package|;
 end_package
 
 begin_import
+import|import static
+name|org
+operator|.
+name|junit
+operator|.
+name|Assert
+operator|.
+name|assertEquals
+import|;
+end_import
+
+begin_import
+import|import static
+name|org
+operator|.
+name|junit
+operator|.
+name|Assert
+operator|.
+name|assertFalse
+import|;
+end_import
+
+begin_import
+import|import static
+name|org
+operator|.
+name|junit
+operator|.
+name|Assert
+operator|.
+name|assertNotNull
+import|;
+end_import
+
+begin_import
+import|import static
+name|org
+operator|.
+name|junit
+operator|.
+name|Assert
+operator|.
+name|assertNull
+import|;
+end_import
+
+begin_import
+import|import static
+name|org
+operator|.
+name|junit
+operator|.
+name|Assert
+operator|.
+name|assertTrue
+import|;
+end_import
+
+begin_import
+import|import static
+name|org
+operator|.
+name|junit
+operator|.
+name|Assert
+operator|.
+name|fail
+import|;
+end_import
+
+begin_import
 import|import
 name|java
 operator|.
@@ -44,16 +116,6 @@ operator|.
 name|util
 operator|.
 name|HashSet
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|util
-operator|.
-name|List
 import|;
 end_import
 
@@ -391,18 +453,6 @@ name|LoggerFactory
 import|;
 end_import
 
-begin_import
-import|import static
-name|org
-operator|.
-name|junit
-operator|.
-name|Assert
-operator|.
-name|*
-import|;
-end_import
-
 begin_class
 specifier|public
 class|class
@@ -683,11 +733,7 @@ block|}
 block|}
 annotation|@
 name|Test
-argument_list|(
-name|timeout
-operator|=
-literal|30000
-argument_list|)
+comment|// (timeout=30000)
 specifier|public
 name|void
 name|testAnonymousProducerConsume
@@ -706,171 +752,30 @@ name|createConnection
 argument_list|()
 expr_stmt|;
 block|{
-name|Session
-name|session
-init|=
-name|connection
-operator|.
-name|createSession
-argument_list|(
-literal|false
-argument_list|,
-name|Session
-operator|.
-name|AUTO_ACKNOWLEDGE
-argument_list|)
-decl_stmt|;
-name|Queue
-name|queue1
-init|=
-name|session
-operator|.
-name|createQueue
-argument_list|(
-name|getDestinationName
-argument_list|()
-operator|+
-literal|"1"
-argument_list|)
-decl_stmt|;
-name|Queue
-name|queue2
-init|=
-name|session
-operator|.
-name|createQueue
-argument_list|(
-name|getDestinationName
-argument_list|()
-operator|+
-literal|"2"
-argument_list|)
-decl_stmt|;
-name|MessageProducer
-name|p
-init|=
-name|session
-operator|.
-name|createProducer
-argument_list|(
-literal|null
-argument_list|)
-decl_stmt|;
-name|TextMessage
-name|message
-init|=
-name|session
-operator|.
-name|createTextMessage
-argument_list|()
-decl_stmt|;
-name|message
-operator|.
-name|setText
-argument_list|(
-literal|"hello"
-argument_list|)
-expr_stmt|;
-name|p
-operator|.
-name|send
-argument_list|(
-name|queue1
-argument_list|,
-name|message
-argument_list|)
-expr_stmt|;
-name|p
-operator|.
-name|send
-argument_list|(
-name|queue2
-argument_list|,
-name|message
-argument_list|)
-expr_stmt|;
-block|{
-name|MessageConsumer
-name|consumer
-init|=
-name|session
-operator|.
-name|createConsumer
-argument_list|(
-name|queue1
-argument_list|)
-decl_stmt|;
-name|Message
-name|msg
-init|=
-name|consumer
-operator|.
-name|receive
-argument_list|(
-name|TestConfig
-operator|.
-name|TIMEOUT
-argument_list|)
-decl_stmt|;
-name|assertNotNull
-argument_list|(
-name|msg
-argument_list|)
-expr_stmt|;
-name|assertTrue
-argument_list|(
-name|msg
-operator|instanceof
-name|TextMessage
-argument_list|)
-expr_stmt|;
-name|consumer
-operator|.
-name|close
-argument_list|()
-expr_stmt|;
-block|}
-block|{
-name|MessageConsumer
-name|consumer
-init|=
-name|session
-operator|.
-name|createConsumer
-argument_list|(
-name|queue2
-argument_list|)
-decl_stmt|;
-name|Message
-name|msg
-init|=
-name|consumer
-operator|.
-name|receive
-argument_list|(
-name|TestConfig
-operator|.
-name|TIMEOUT
-argument_list|)
-decl_stmt|;
-name|assertNotNull
-argument_list|(
-name|msg
-argument_list|)
-expr_stmt|;
-name|assertTrue
-argument_list|(
-name|msg
-operator|instanceof
-name|TextMessage
-argument_list|)
-expr_stmt|;
-name|consumer
-operator|.
-name|close
-argument_list|()
-expr_stmt|;
-block|}
+comment|//            Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+comment|//            Queue queue1 = session.createQueue(getDestinationName() + "1");
+comment|//            Queue queue2 = session.createQueue(getDestinationName() + "2");
+comment|//            MessageProducer p = session.createProducer(null);
+comment|//
+comment|//            TextMessage message = session.createTextMessage();
+comment|//            message.setText("hello");
+comment|//            p.send(queue1, message);
+comment|//            p.send(queue2, message);
+comment|//
+comment|//            {
+comment|//                MessageConsumer consumer = session.createConsumer(queue1);
+comment|//                Message msg = consumer.receive(TestConfig.TIMEOUT);
+comment|//                assertNotNull(msg);
+comment|//                assertTrue(msg instanceof TextMessage);
+comment|//                consumer.close();
+comment|//            }
+comment|//            {
+comment|//                MessageConsumer consumer = session.createConsumer(queue2);
+comment|//                Message msg = consumer.receive(TestConfig.TIMEOUT);
+comment|//                assertNotNull(msg);
+comment|//                assertTrue(msg instanceof TextMessage);
+comment|//                consumer.close();
+comment|//            }
 block|}
 block|}
 annotation|@
