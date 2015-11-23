8db797bbee8fbce5bc49d557b6c118c4f01ac259
Timothy Bish <tabish121@gmail.com>
2015-11-23 16:55:11 -0500

    https://issues.apache.org/jira/browse/AMQ-6044
    Clean up some test client code.
    
    Former-commit-id: d9e22a9368cdaa67501374a41e9ccafc2629f488

--- activemq-amqp/src/test/java/org/apache/activemq/transport/amqp/client/AmqpResource.java
+++ activemq-amqp/src/test/java/org/apache/activemq/transport/amqp/client/AmqpResource.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+comment|/*  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -18,16 +18,6 @@ operator|.
 name|client
 package|;
 end_package
-
-begin_import
-import|import
-name|java
-operator|.
-name|io
-operator|.
-name|IOException
-import|;
-end_import
 
 begin_import
 import|import
@@ -57,6 +47,8 @@ begin_interface
 specifier|public
 interface|interface
 name|AmqpResource
+extends|extends
+name|AmqpEventSink
 block|{
 comment|/**      * Perform all the work needed to open this resource and store the request      * until such time as the remote peer indicates the resource has become active.      *      * @param request      *        The initiating request that triggered this open call.      */
 name|void
@@ -122,71 +114,6 @@ parameter_list|(
 name|Exception
 name|cause
 parameter_list|)
-function_decl|;
-comment|/**      * Event handler for remote peer open of this resource.      *      * @param connection      *        The connection that owns this resource.      *      * @throws IOException if an error occurs while processing the update.      */
-name|void
-name|processRemoteOpen
-parameter_list|(
-name|AmqpConnection
-name|connection
-parameter_list|)
-throws|throws
-name|IOException
-function_decl|;
-comment|/**      * Event handler for remote peer detach of this resource.      *      * @param connection      *        The connection that owns this resource.      *      * @throws IOException if an error occurs while processing the update.      */
-name|void
-name|processRemoteDetach
-parameter_list|(
-name|AmqpConnection
-name|connection
-parameter_list|)
-throws|throws
-name|IOException
-function_decl|;
-comment|/**      * Event handler for remote peer close of this resource.      *      * @param connection      *        The connection that owns this resource.      *      * @throws IOException if an error occurs while processing the update.      */
-name|void
-name|processRemoteClose
-parameter_list|(
-name|AmqpConnection
-name|connection
-parameter_list|)
-throws|throws
-name|IOException
-function_decl|;
-comment|/**      * Called when the Proton Engine signals an Delivery related event has been triggered      * for the given endpoint.      *      * @param connection      *        The connection that owns this resource.      *      * @throws IOException if an error occurs while processing the update.      */
-name|void
-name|processDeliveryUpdates
-parameter_list|(
-name|AmqpConnection
-name|connection
-parameter_list|)
-throws|throws
-name|IOException
-function_decl|;
-comment|/**      * Called when the Proton Engine signals an Flow related event has been triggered      * for the given endpoint.      *      * @param connection      *        The connection that owns this resource.      *      * @throws IOException if an error occurs while processing the update.      */
-name|void
-name|processFlowUpdates
-parameter_list|(
-name|AmqpConnection
-name|connection
-parameter_list|)
-throws|throws
-name|IOException
-function_decl|;
-comment|/**      * @returns true if the remote end has sent an error      */
-name|boolean
-name|hasRemoteError
-parameter_list|()
-function_decl|;
-comment|/**      * @return an Exception derived from the error state of the endpoint's Remote Condition.      */
-name|Exception
-name|getRemoteError
-parameter_list|()
-function_decl|;
-comment|/**      * @return an Error message derived from the error state of the endpoint's Remote Condition.      */
-name|String
-name|getRemoteErrorMessage
-parameter_list|()
 function_decl|;
 block|}
 end_interface
