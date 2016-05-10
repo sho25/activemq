1a86f1acf5beff0dfea63f0c2505cfad79f82919
Timothy Bish <tabish121@gmail.com>
2016-05-10 10:05:05 -0400

    https://issues.apache.org/jira/browse/AMQ-5621
    Remove hard coded port as the bridge brokers method doesn't need the
    port to be fixed.  Turn off JMX since the test doesn't use it.  
    
    Former-commit-id: 3da9b0720efa4f8c55b8d04d3b584637a25ac1da

--- activemq-unit-tests/src/test/java/org/apache/activemq/bugs/AMQ4147Test.java
+++ activemq-unit-tests/src/test/java/org/apache/activemq/bugs/AMQ4147Test.java
@@ -1,6 +1,6 @@
 begin_unit|revision:0.9.5;language:Java;cregit-version:0.0.1
 begin_comment
-comment|/**  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
+comment|/*  * Licensed to the Apache Software Foundation (ASF) under one or more  * contributor license agreements.  See the NOTICE file distributed with  * this work for additional information regarding copyright ownership.  * The ASF licenses this file to You under the Apache License, Version 2.0  * (the "License"); you may not use this file except in compliance with  * the License.  You may obtain a copy of the License at  *  *      http://www.apache.org/licenses/LICENSE-2.0  *  * Unless required by applicable law or agreed to in writing, software  * distributed under the License is distributed on an "AS IS" BASIS,  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  * See the License for the specific language governing permissions and  * limitations under the License.  */
 end_comment
 
 begin_package
@@ -182,7 +182,7 @@ argument_list|(
 operator|new
 name|URI
 argument_list|(
-literal|"broker:(vm://broker1)/broker1?persistent=false"
+literal|"broker:(vm://broker1)/broker1?persistent=false&useJmx=false"
 argument_list|)
 argument_list|)
 decl_stmt|;
@@ -194,7 +194,7 @@ argument_list|(
 operator|new
 name|URI
 argument_list|(
-literal|"broker:(vm://broker2)/broker2?persistent=false"
+literal|"broker:(vm://broker2)/broker2?persistent=false&useJmx=false"
 argument_list|)
 argument_list|)
 decl_stmt|;
@@ -468,8 +468,7 @@ expr_stmt|;
 block|}
 finally|finally
 block|{
-comment|// Consume the message and verify that there is no more memory
-comment|// usage.
+comment|// Consume the message and verify that there is no more memory usage.
 name|consumerProceed
 operator|.
 name|release
@@ -571,7 +570,7 @@ argument_list|(
 operator|new
 name|URI
 argument_list|(
-literal|"broker:(vm://broker1)/broker1?persistent=false"
+literal|"broker:(vm://broker1)/broker1?persistent=false&useJmx=false"
 argument_list|)
 argument_list|)
 decl_stmt|;
@@ -583,7 +582,7 @@ argument_list|(
 operator|new
 name|URI
 argument_list|(
-literal|"broker:(tcp://localhost:61616)/broker2?persistent=false"
+literal|"broker:(tcp://localhost:0)/broker2?persistent=false&useJmx=false"
 argument_list|)
 argument_list|)
 decl_stmt|;
@@ -855,8 +854,7 @@ expr_stmt|;
 block|}
 finally|finally
 block|{
-comment|// Consume the message and verify that there is no more memory
-comment|// usage.
+comment|// Consume the message and verify that there is no more memory usage.
 name|consumerProceed
 operator|.
 name|release
