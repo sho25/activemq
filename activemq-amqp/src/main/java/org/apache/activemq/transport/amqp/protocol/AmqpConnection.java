91582125ace476699b9921cd205208bb94ddafc3
Timothy Bish <tabish121@gmail.com>
2016-05-26 16:52:18 -0400

    Remove unused import.
    Former-commit-id: 08f266d7629e079727af763342ddc2b6d573d3f4

--- activemq-amqp/src/main/java/org/apache/activemq/transport/amqp/protocol/AmqpConnection.java
+++ activemq-amqp/src/main/java/org/apache/activemq/transport/amqp/protocol/AmqpConnection.java
@@ -291,16 +291,6 @@ name|java
 operator|.
 name|util
 operator|.
-name|Date
-import|;
-end_import
-
-begin_import
-import|import
-name|java
-operator|.
-name|util
-operator|.
 name|HashMap
 import|;
 end_import
