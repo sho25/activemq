3f5a1cfb5a7caed574b271d615ea4038aa433118
gtully <gary.tully@gmail.com>
2018-05-30 22:03:50 +0100

    AMQ-6979 - use scheduler as trigger task - do heavy lifting via the task runner executor, take care to trap errors to keep scheduler timer alive AMQ-5129
    Former-commit-id: cdb38b32756d70bf7ae149e994abcccc9adca5ae

--- activemq-broker/src/main/java/org/apache/activemq/broker/BrokerService.java
+++ activemq-broker/src/main/java/org/apache/activemq/broker/BrokerService.java
@@ -9945,7 +9945,7 @@ expr_stmt|;
 block|}
 catch|catch
 parameter_list|(
-name|Exception
+name|Throwable
 name|e
 parameter_list|)
 block|{
@@ -9967,7 +9967,7 @@ expr_stmt|;
 block|}
 catch|catch
 parameter_list|(
-name|Exception
+name|Throwable
 name|e
 parameter_list|)
 block|{
